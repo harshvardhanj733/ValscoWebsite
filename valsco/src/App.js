dce22dbda8568cdf9ad349df0fad534001b5e328
Rohan Srivastava <rohansrivastava.lko.up@gmail.com>
2023-06-15 14:12:01 +0530

    added hoverimage

--- valsco/src/App.js
+++ valsco/src/App.js
@@ -10,6 +10,7 @@ import DetailedBlog from './components/Blogs/Components/DetailedBlog';
 import Slider from './components/Blogs/Components/Slider';
 import Contact from './components/Contact/Contact';
 import LandingPage from './components/Landing/LandingPage';
+import Hoverimage from './components/hoverImage/Hoverimage';
 
 function App() {
   const [isLoadingComplete, setIsLoadingComplete] = useState(false);
@@ -18,84 +19,17 @@ function App() {
   useEffect(() => {
     setTimeout(() => {
       setIsLoadingComplete(true);
-    }, 2000);
+    }, 4000);
   }, []);
 
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              {!isLoadingComplete ? (
-                <LoadingPage />
-              ) : (
-                <>
-                  <div>
-                  <LandingPage/>
-                    <ProductsPage />
-                    <Slider authenticated={authenticated} />
-                    <Contact />
-                  </div>
-                  <Routes>
-                  <Route
-                    path="/BlogHome"
-                    element={
-                      <>
-                        <Navbar
-                          authenticated={authenticated}
-                          setAuthenticated={setAuthenticated}
-                        />
-                        <BlogHome authenticated={authenticated} />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="/CreateBlog"
-                    element={
-                      <>
-                        <Navbar
-                          authenticated={authenticated}
-                          setAuthenticated={setAuthenticated}
-                        />
-                        <CreateBlog authenticated={authenticated} />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="/Login"
-                    element={
-                      <>
-                        <Navbar
-                          authenticated={authenticated}
-                          setAuthenticated={setAuthenticated}
-                        />
-                        <Login
-                          setAuthenticated={setAuthenticated}
-                          authenticated={authenticated}
-                        />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="/DetailedBlog/:ID"
-                    element={
-                      <>
-                        <Navbar
-                          authenticated={authenticated}
-                          setAuthenticated={setAuthenticated}
-                        />
-                        <DetailedBlog authenticated={authenticated} />
-                      </>
-                    }
-                  />
-                  </Routes>
-                </>
-              )}
-            </>
-          }
-        />
+        <Route path="/" element={<>{!isLoadingComplete ? (<LoadingPage />) : (<><div><LandingPage/><Hoverimage/><ProductsPage /><Slider authenticated={authenticated} /><Contact /> </div></>)}</>}/>
+        <Route path="/BlogHome" element={<><Navbar authenticated={authenticated} setAuthenticated={setAuthenticated}/><BlogHome authenticated={authenticated} /></>}/>
+        <Route path="/CreateBlog" element={<><Navbar authenticated={authenticated} setAuthenticated={setAuthenticated}/><CreateBlog authenticated={authenticated} /></>}/>
+        <Route path="/Login" element={<><Navbar authenticated={authenticated} setAuthenticated={setAuthenticated}/><Login setAuthenticated={setAuthenticated} authenticated={authenticated}/></>}/>
+        <Route path="/DetailedBlog/:ID" element={<><Navbar authenticated={authenticated} setAuthenticated={setAuthenticated}/><DetailedBlog authenticated={authenticated} /></>}/>
       </Routes>
     </Router>
   );
