af90ad51848bb91d1c004bb03af9c9a2751b8a99
harshvardhanj733 <harshvardhanj733@gmail.com>
2023-07-05 18:34:39 +0530

    Contact Form Backend Working

--- valsco/src/components/Contact/Contact.js
+++ valsco/src/components/Contact/Contact.js
@@ -1,7 +1,88 @@
 import './Contact.css';
-import React from 'react';
+import React, {useState} from 'react';
 
 function Contact() {
+
+  const [newContact, setNewContact] = useState({ name: '', email: '', number: '', company: '' })
+
+  function ValidateEmail(mail) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+      return true
+    }
+    return false
+  }
+
+  function phonenumber(inputtxt) {
+    var phoneno = /^\d{10}$/;
+    if (inputtxt.match(phoneno)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  const handleOnChange = (e) => {
+    setNewContact({ ...newContact, [e.target.name]: e.target.value })
+  }
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+
+    const url = "https://website-bad9f-default-rtdb.asia-southeast1.firebasedatabase.app/contactUsRecords.json"
+
+    try {
+      if (ValidateEmail(newContact.email) && phonenumber(newContact.number) && newContact.name.length >= 3 && newContact.company.length >= 3) {
+        const response = await fetch(url, {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            'Content-Type': "application/json"
+          },
+          body: JSON.stringify({ name: newContact.name, email: newContact.email, number: newContact.number, company: newContact.company })
+        })
+
+        let savedEmail = newContact.email;
+        let savedName = newContact.name;
+        setNewContact({ name: '', email: '', number: '', company: '' });
+        const url2 = "http://localhost:5000";
+        const response2 = await fetch(url2, {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            'Content-Type': "application/json"
+          },
+          body: JSON.stringify({ name: savedName, email: savedEmail })
+        })
+
+      }
+      else {
+        if (newContact.name.length < 3) {
+          alert("Please Fill The Name Correctly. Name Should Be Atleast 3 Characters Long");
+        }
+        else if (!ValidateEmail(newContact.email)) {
+          alert("Please Fill The Email Correctly")
+        }
+        else if (!phonenumber(newContact.number)) {
+
+          if (!isNaN(newContact.number)) {
+            alert(`Please Fill Phone Number Correctly. Phone Number Must Be Only 10 Digits Long. Currently You Have Filled ${newContact.number.length} digits.`)
+          }
+          else {
+            alert('Please Fill Phone Number Correctly. Phone Number Must Be Only 10 Digits Long. It should not contain any Alphabets or Special Characters.')
+          }
+        }
+        else {
+          alert("Company Should Be Atleast 3 Characters Long")
+        }
+      }
+
+    } catch (error) {
+      alert(`The Following Error Occured: ${error}.\nKindly Try Again!`)
+      setNewContact({ name: '', email: '', number: '', company: '' });
+    }
+  }
+
   return (
     <div className="complete">
       <div className="App">
@@ -81,6 +162,9 @@ function Contact() {
                 className="form-control"
                 id="inputEmail4"
                 placeholder="Your Name"
+                onChange={handleOnChange}
+                name='name'
+                value={newContact.name}
               />
             </div>
             <div className="form-row">
@@ -90,6 +174,9 @@ function Contact() {
                 className="form-control"
                 id="inputEmail4"
                 placeholder="Your Email"
+                onChange={handleOnChange}
+                name='email'
+                value={newContact.email}
               />
             </div>
             <div className="form-row">
@@ -99,6 +186,9 @@ function Contact() {
                 className="form-control"
                 id="inputAddress2"
                 placeholder="Your Contact number"
+                onChange={handleOnChange}
+                name='number'
+                value={newContact.number}
               />
             </div>
             <div className="form-row">
@@ -108,9 +198,12 @@ function Contact() {
                 className="form-control"
                 id="inputCity"
                 placeholder="Your Company/Organisation"
+                onChange={handleOnChange}
+                name='company'
+                value={newContact.company}
               />
             </div>
-            <button type="submit" className="btn btn-primary transparent-button" id = "buton">
+            <button type="submit" className="btn btn-primary transparent-button" id="buton" onClick={handleOnSubmit}>
               Click to send your message
             </button>
           </form>
@@ -137,7 +230,7 @@ function Contact() {
           </div>
         </div>
       </div>
-</div>
+    </div>
 
   );
 }
---
+++ valsco_backend/email.js
@@ -0,0 +1,13 @@
+const nodemailer = require('nodemailer')
+require("dotenv").config()
+
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    },
+    pool: true
+});
+
+module.exports = transporter;
