d932f2391f1e214bc970842fd6833ca09aa5f965
NIMIT <nimitj2003@gmail.com>
2023-07-22 14:17:54 +0530

    newproductspage

---
+++ products (1)/src/pages/IPhone14ProSpaceBlackMock.js
@@ -0,0 +1,16 @@
+import "./IPhone14ProSpaceBlackMock.css";
+const IPhone14ProSpaceBlackMock = () => {
+  return (
+    <div className="iphone-14-pro-space-black-mock">
+      <img className="shadow-icon1" alt="" src="/shadow@2x.png" />
+      <img className="main-icon1" alt="" src="/main@2x.png" />
+      <img
+        className="iphone-14-pro-space-black-mock3"
+        alt=""
+        src="/iphone-14-pro-space-black-mockup-label@2x.png"
+      />
+    </div>
+  );
+};
+
+export default IPhone14ProSpaceBlackMock;
---
+++ products (1)/src/pages/IPhone14ProSpaceBlackMock2.js
@@ -0,0 +1,16 @@
+import "./IPhone14ProSpaceBlackMock2.css";
+const IPhone14ProSpaceBlackMock2 = () => {
+  return (
+    <div className="iphone-14-pro-space-black-mock4">
+      <img className="shadow-icon2" alt="" src="/shadow@2x.png" />
+      <img className="main-icon2" alt="" src="/main@2x.png" />
+      <img
+        className="iphone-14-pro-space-black-mock5"
+        alt=""
+        src="/iphone-14-pro-space-black-mockup-label2@2x.png"
+      />
+    </div>
+  );
+};
+
+export default IPhone14ProSpaceBlackMock2;
