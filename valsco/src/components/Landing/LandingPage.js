042bcbc06249ba62d2ab4ae20f99e2253d412d68
Rohan Srivastava <rohansrivastava.lko.up@gmail.com>
2023-08-08 19:39:32 +0530

    finally

--- valsco/src/components/Landing/LandingPage.js
+++ valsco/src/components/Landing/LandingPage.js
@@ -1,36 +1,37 @@
 import "./LandingPage.css"
 import React from "react"
-import imgvalsco from '../images/img2.png'
+import imgvalsco from "../images/img2.png"
 import { Link } from "react-router-dom"
 
 function LandingPage() {
   return (
     <>
-
       <div className="wrapper">
         <div className="main">
           <div className="front">
             <div className="img1">
-              <img src={require("../../components/images/newlogo.png")} id="lgo" alt="gb" />
+              <img
+                src={require("../../components/images/newlogo.png")}
+                id="lgo"
+                alt="gb"
+              />
             </div>
           </div>
           <div className="para">
-              <div><h1>VALSCO</h1></div>
-              <div className="para1"><p>
-                Our company provides software products and services to businesses
-                individuals to help them meet their software needs. We strive to
-                specialize in developing software solutions tailored to the unique
-                needs of our clients.
-              </p>
-              </div>
-            </div>
+            <span>VALSCO</span>
+            <p>
+              Our company provides software products and services to businesses
+              individuals to help them meet their software needs. We strive to
+              specialize in developing software solutions tailored to the unique
+              needs of our clients.
+            </p>
+          </div>
         </div>
       </div>
 
       {/* <div className='imghovercontainer'>
       <img className="imghover" src={imgvalsco} alt="" />
     </div> */}
-
     </>
   )
 }
